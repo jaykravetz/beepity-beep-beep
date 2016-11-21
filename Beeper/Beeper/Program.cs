67d998b452a8aa58fe26eb767365b68fdbecd3cc
Mordy Tikotzky <mordytk@gmail.com>
2016-11-21 11:54:41 -0500

    [Fix] Grammar

--- Beeper/Beeper/Program.cs
+++ Beeper/Beeper/Program.cs
@@ -18,7 +18,7 @@ namespace Beeper
                 System.Threading.Thread.Sleep(800);
             }
 
-            Console.WriteLine("Im done beeping now");
+            Console.WriteLine("I'm done beeping now.");
 
             System.Threading.Thread.Sleep(2000);
 
