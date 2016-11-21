a5e7e8ddd69f8b158500671f21d9e93208b358fd
Jay Kravetz <jayk@ltccs.com>
2016-11-21 11:39:04 -0500

    So it begins

---
+++ Beep.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beeper
+{
+    class Beep
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("Beep!");
+                Console.Beep();
+
+                System.Threading.Thread.Sleep(800);
+            }
+
+            Console.WriteLine("Im done beeping now");
+
+            System.Threading.Thread.Sleep(2000);
+
+        }
+    }
+}
