c24027d9330be68fb271093cae62ef4ac442f030
Jay Kravetz <jay@jkravetz.net>
2016-11-21 11:48:43 -0500

    Create a VS project

---
+++ Beeper/Beeper/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("Beep!");
+                Console.Beep();
+
+                System.Threading.Thread.Sleep(800);
+            }
+
+            Console.WriteLine("Im done beeping now");
+
+            System.Threading.Thread.Sleep(2000);
+
+        }
+    }
+}
